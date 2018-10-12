1c99d892884867b721eba4c9ab3f4bfe9ed55489
Johan Navarro <21sebas12@gmail.com>
2018-10-12 12:40:51 -0500

    Se crea todos los componentes para crear el simulador adema de establecer el funcionamiento de zoom

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 
 // import PDFsimulate from './components/containers/PDFsimulate.jsx'
 // export default PDFsimulate;
-
-
-import React from 'react';
-class Fancy extends React.Component {
-  render() {
-    return (
-      <div>This is so Fancy!</div>
-    );
-  }
-}
-export default Fancy;
